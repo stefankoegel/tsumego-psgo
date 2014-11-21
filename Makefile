88b30c4f43a99534fc1e3b17da6119a8d5aeacf3
Stefan Kögel <stefan.koegel@uni-ulm.de>
2014-11-21 21:02:38 +0100

    Add parser for .tex files
    Update Makefile and .gitignore accordingly

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 
-.PHONY: clean
+.PHONY: all, clean
+
+all: bin/reformatter
 
 tsumego/books/cho-1-elementary.tex:
 	git clone https://github.com/tasuk/tsumego.git
 
+bin/reformatter: src/reformatter.hs
+	ghc $< -o $@ -outputdir tmp
+
 clean:
 	rm -rf tsumego
+	rm tmp/*
--- src/reformatter.hs
+++ src/reformatter.hs
@@ -1,3 +1,52 @@
 module Main where
 
+import System.Environment
+import Text.Parsec
+import Control.Applicative ((<*))
+
 main = putStrLn "Hello world!"
+
+data Intersection = Black | White | Free
+    deriving Show
+
+type SimpleBoard = (String, [[Intersection]])
+
+type Position = (Intersection, (Char, Int))
+type PositionBoard = [(String, [Position])]
+
+-- *** Parser ***
+
+intersection :: Parsec String u Intersection
+intersection = do
+    string "\\"
+    inter <-
+            try (string "- @" >> return Black)
+        <|> try (string "- !" >> return White)
+        <|>     (string "0??" >> return Free)
+    oneOf "<>[](*+"
+    return inter
+
+intersections :: Parsec String u [[Intersection]]
+intersections = many (many intersection <* newline)
+
+beforeBoard :: Parsec String u ()
+beforeBoard = do
+    manyTill anyChar $ try $ string "\\vbox{\\vbox" >> manyTill anyChar newline
+    return ()
+
+boardName :: Parsec String u String
+boardName = do
+    char '}' >> newline
+    string "\\hfil "
+    manyTill anyChar (try $ string "\\hfil\\break" >> newline)
+
+
+simpleBoard :: Parsec String u SimpleBoard
+simpleBoard = do
+    beforeBoard
+    inters <- intersections
+    name <- boardName
+    return (name, inters)
+
+simpleBoards :: Parsec String u [SimpleBoard]
+simpleBoards = many $ try simpleBoard
